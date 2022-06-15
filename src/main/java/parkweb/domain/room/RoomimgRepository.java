b245a6e115ccf725fa653a718ee5e5106fa88f31
504 <lvbluechair@gmail.com>
2022-06-15 11:31:07 +0900

    DB 수정1

---
+++ src/main/java/parkweb/domain/room/RoomimgRepository.java
@@ -0,0 +1,7 @@
+package parkweb.domain.room;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomimgRepositoy extends JpaRepository<RoomimgRepositoy,Integer> {
+
+}
